e8af7439882fb4d436e5b2a4abacbb3104bc5988
ignant <ignanttt@gmail.com>
2026-02-24 22:00:22 +0100

    UPDATED | Secondary-Enemy Logic for level 1 created | dummy object to work as player created

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 # Orchestrator (game flow)
 import pygame
 
-from Game.settings import WIDTH, HEIGHT, FPS
-from Game.menu import draw_main_screen, START_RECT
-from Game.level_1 import draw_level
+import Game.settings as settings
+import Game.menu as menu
+import Game.Level_1.level_1 as level_1
 
 pygame.init()
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
 pygame.display.set_caption("Game name")
 clock = pygame.time.Clock()
+
+level_1.load_assets()
 
 running = True
 main_menu = True
 
 while running:
+    dt = clock.tick(settings.FPS) / 1000.0
+    keys = pygame.key.get_pressed() # listen to keys to be pressed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if main_menu and START_RECT.collidepoint(event.pos):
-                main_menu = False
+        if main_menu:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if menu.START_RECT.collidepoint(event.pos):
+                    main_menu = False
+                    level_1.start_level()
+        else:
+            level_1.handle_level_event(event)
 
     # draw current state
     if main_menu:
-        draw_main_screen(screen)
+        menu.draw_main_screen(screen)
     else:
-        draw_level(screen)
+        level_1.update_level(dt, keys)
+        level_1.draw_level(screen)
 
     pygame.display.update()
-    clock.tick(FPS)
 
 pygame.quit()
