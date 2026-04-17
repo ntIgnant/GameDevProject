19fdcff8198a9e4acd6b9c3d45d4fa2144d9d413
Sabrina <sabrina.bran16@gmail.com>
2026-04-17 19:00:27 +0200

    updated countdown

--- Game/timer.py
+++ Game/timer.py
@@ -1,18 +1,27 @@
 import pygame
 import Game.settings as settings
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+ASSETS_DIR = os.path.join(BASE_DIR, "Assets", "pause_menu")
+
+TABLE_PATH = os.path.join(ASSETS_DIR, "Table.png")
 
 pygame.init()
 
 class Timer():
     def __init__(self, minutes):
         self.seconds = minutes * 60 # total seconds
-        self.font = pygame.font.SysFont(None, 100)
+        self.font = pygame.font.SysFont(None, 80)
         self.clock = pygame.time.Clock()
         self.run = True
-        self.colour = (51, 153, 255) #TODO: find a better colour
+        self.colour = (250, 250, 250) #TODO: find a better colour
         self.timer_event = pygame.USEREVENT + 1 # create an event for the timer ticks
         pygame.time.set_timer(self.timer_event, 1000) # the event happens every second (1000 milliseconds = 1 second)
         self.text = self.font.render(self.time_format(), True, self.colour)
+        self.background_img = pygame.image.load(TABLE_PATH)
+        self.background_img = pygame.transform.scale(self.background_img, (250, 80))
+        self.background_rect = self.background_img.get_rect(center=(settings.WIDTH//2, 50))
 
     def time_format(self):
         # returning the countdown string using the format MM:SS
@@ -31,6 +40,8 @@ class Timer():
                 self.text = self.font.render(self.time_format(), True, self.colour)        
             
     def draw(self, screen):
-        text_rect = self.text.get_rect(center = (settings.WIDTH // 2, 50))
+        screen.blit(self.background_img, self.background_rect)
+        
+        text_rect = self.text.get_rect(center = self.background_rect.center)
         screen.blit(self.text, text_rect)
 
